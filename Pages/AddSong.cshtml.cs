0c759671d012d9041a2db884039862c9a23b569a
andrewj427 <aajohnston@email.neit.edu>
2026-08-05 01:13:51 -0400

    Create Frontend for website

---
+++ Pages/AddSong.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PracticeVault.Pages
+{
+    public class AddSongModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
